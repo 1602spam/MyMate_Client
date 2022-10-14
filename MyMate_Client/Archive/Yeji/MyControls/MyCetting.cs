6866f5c3ce0ca03fad3e011ab3c6882a49ce2ee8
yeji <kimyeji0045@naver.com>
2022-10-14 20:30:13 +0900

    Archive

---
+++ MyMate_Client/Archive/Yeji/MyControls/MyCetting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yeji.MyControls
+{
+    public partial class MyChetting : UserControl
+    {
+        private Image ?img;
+
+        public MyChetting()
+        {
+            InitializeComponent();
+        }
+
+        public string Message
+        {
+            get
+            {
+                return MsgText.Text;
+            }
+            set
+            {
+                MsgText.Text = value;
+            }
+        }
+        
+
+        void AdjustHeight()
+        {
+            
+            MsgText.Height = Utility.GetTextHeight(MsgText) + 10;
+
+            Bubble.Height = MsgText.Top + Bubble.Top + MsgText.Top;
+
+            this.Height = Bubble.Bottom + 10;
+        }
+
+       
+
+        private void MyChetting_DockChanged(object sender, EventArgs e)
+        {
+            AdjustHeight();
+        }
+
+        private void MyChetting_Resize(object sender, EventArgs e)
+        {
+            AdjustHeight();
+        }
+    }
+}
---
+++ MyMate_Client/Archive/Yeji/View/Controls/ChettingPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yeji.View.Controls
+{
+    public class ChettingPanel : Panel
+    {
+        public ChettingPanel()
+        {
+            this.BackColor = Color.FromArgb(242, 242, 242);
+            this.Dock = DockStyle.Fill;
+            Location = new Point(150, 0);
+
+        }
+    }
+}

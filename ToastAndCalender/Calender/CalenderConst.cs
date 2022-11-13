0439a6985beeeead31e361a1decab9e6f74bf481
poro912 <40479447+poro912@users.noreply.github.com>
2022-11-13 19:59:59 +0900

    1차 완성

---
+++ ToastAndCalender/Calender/CalenderConst.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calender
+{
+	public static class CalendarSetting
+	{
+		// 캘린더
+		public const int TitleHeight = 30;
+
+		public const int CalendarMinHeight = 5 * DayMinHeight;
+		public const int CalendarMinWidth = 7 * scheduleWidth;
+
+		// 날짜
+		// 날짜 표시 높이
+		public const int DayTitleHeight = 30;
+		// 최소 길이
+		public const int DayMinWidth = scheduleWidth;
+		// 최소 높이
+		public const int DayMinHeight = DayTitleHeight + 2 * scheduleItemHeight + scheduleHeightGap;
+
+		// 스케줄
+		// 스케줄 크기
+		public const int scheduleHeight = 20;
+		public const int scheduleWidth = 100;
+
+		// 스케줄끼리의 간격
+		public const int scheduleHeightGap = 2;
+		public const int scheduleWidhtGap = 10;
+
+		// 크기 + 간격
+		public const int scheduleItemHeight = scheduleHeight + scheduleHeightGap;
+		public const int scheduleItemWidth = scheduleWidth + scheduleWidhtGap;
+	}
+	public static class ScheduleState
+	{
+		public const int BodyOnly = 0;
+		public const int Head = 1;
+		public const int Body = 2;
+		public const int Tail = 3;
+	}
+}
---
+++ ToastAndCalender/Calender/CalenderDay.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calender
+{
+	public partial class CalenderDay : UserControl
+	{
+		public Dictionary<int, ScheduleBar> scheduleBars;
+		int depth;
+		int Depth
+		{
+			get { return depth; }
+			set { this.CheckDapth(value);}
+		}
+		public int X
+		{
+			get{return this.Location.X;}
+			set{this.Location = new Point(value, Y);}
+
+		}
+		public int Y
+		{
+			get{return this.Location.Y;}
+			set{this.Location = new Point(X, value);}
+		}
+		public string Title
+		{
+			set{this.lblDay.Text = value;}
+		}
+
+		public CalenderDay()
+		{
+			InitializeComponent();
+			this.scheduleBars = new();
+		}
+		private void CalenderDay_Load(object sender, EventArgs e)
+		{
+			this.Size = new Size(CalendarSetting.DayMinWidth, CalendarSetting.DayMinHeight);
+			this.depth = 0;
+		}
+
+		public void Clear()
+		{
+			this.scheduleBars.Clear();
+			// 사이즈 최소로 지정
+			this.Size = new Size(CalendarSetting.DayMinWidth, CalendarSetting.DayMinHeight);
+			this.depth = 0;
+		}
+
+		public void CheckDapth(int n = 0)
+		{
+
+			int mostHeight = 0;
+			if (this.scheduleBars.Count > 1)
+				mostHeight = this.scheduleBars.Aggregate((x, y) => x.Key > y.Key ? x : y).Key;
+			// 더 큰 것 저장
+			mostHeight = mostHeight > n ? mostHeight : n;
+			depth = mostHeight > depth ? mostHeight : depth;
+			ReSize();
+		}
+
+		public void ReSize()
+		{
+			if( 2 >= depth )
+			{
+				this.Size = new Size(CalendarSetting.DayMinWidth, CalendarSetting.DayMinHeight);
+			}
+
+			int y = CalendarSetting.DayTitleHeight + CalendarSetting.scheduleHeightGap;
+
+			this.Height = y + ((depth + 1) * CalendarSetting.scheduleItemHeight);
+			this.panel.Dock = DockStyle.Fill;
+			Replace();
+		}
+		public void Replace()
+		{
+			int y = CalendarSetting.DayTitleHeight + CalendarSetting.scheduleHeightGap;
+			if (scheduleBars.Count <= 0)
+				return;
+			foreach (var item in scheduleBars)
+			{
+				item.Value.Y = y + item.Key * CalendarSetting.scheduleItemHeight;
+			}
+		}
+		public void Add(int dapth, ScheduleBar SB)
+		{
+			SB.Visible = true;
+			this.panel.Controls.Add(SB);
+			this.scheduleBars.Add(dapth, SB);
+			CheckDapth();
+		}
+		public void RemoveComponet(int n)
+		{
+			this.scheduleBars.Remove(n);
+			CheckDapth();
+		}
+
+		public int MaxIndex()
+		{
+			return this.scheduleBars.Aggregate((x, y) => x.Key > y.Key ? x : y).Key;
+		}
+
+		// 해당 높이에 값이 있는지 확인
+		public bool Check(int n)
+		{
+			return this.scheduleBars.ContainsKey(n);
+		}
+
+		private void lblDay_Click(object sender, EventArgs e)
+		{
+
+		}
+
+	}
+}

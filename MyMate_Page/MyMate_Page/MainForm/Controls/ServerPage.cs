ae320bcbd8e122623f899b0eb3b911a82605fdcd
1602spam <1602spam@naver.com>
2022-11-15 20:11:04 +0900

    test ready

--- MyMate_Page/MyMate_Page/MainForm/Controls/ServerPage.cs
+++ MyMate_Page/MyMate_Page/MainForm/Controls/ServerPage.cs
@@ -42,6 +42,7 @@ namespace MainForm.Controls
             SC = new ServerChat();
             SC.Parent = SChatPanel;
             SC.Dock = DockStyle.Fill;
+            SC.Initialize
 
             SChatListPanel.Controls.Add(SCL);
             SChatPanel.Controls.Add(SC);
